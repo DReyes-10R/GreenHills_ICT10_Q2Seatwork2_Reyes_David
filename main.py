65170daf813a9324baa33c8fb52de1b6f5de9a37
David <reyes.da@obmontessori.edu.ph>
2026-01-09 08:55:28 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ def calc_gwa(e=None):
     first = document.getElementById("first_name").value
     last = document.getElementById("last_name").value
 
+    
     def to_float(idname):
         try:
             return float(document.getElementById(idname).value)
         except:
             return 0.0
+
 
     science = to_float("g_science")
     math = to_float("g_math")
@@ -20,11 +22,23 @@ def calc_gwa(e=None):
     total = science + math + english + filipino + ict + pe
     average = total / 6
 
-    remark = "Passed" if average >= 75 else "Failed"
+    # IF–ELSE conditional statement
+    if average > 74:
+        remark = "PASSED"
+    else:
+        remark = "FAILED"
 
+   
     document.getElementById("name_out").innerText = f"{first} {last}"
     document.getElementById("summary_out").innerText = (
-        f"Science: {science}\nMath: {math}\nEnglish: {english}\n"
-        f"Filipino: {filipino}\nICT: {ict}\nPE: {pe}"
+        f"Science: {science}"
+        f"Math: {math}"
+        f"English: {english}"
+        f"Filipino: {filipino}"
+        f"ICT: {ict}"
+        f"PE: {pe}"
     )
-    document.getElementById("gwa_out").innerText = f"Your General Weighted Average (GWA) is : {average:.2f} ({remark})"
+    document.getElementById("gwa_out").innerText = (
+        f"Your General Weighted Average (GWA) is: {average:.2f}"
+        f"Status?: {remark}"
+    )
